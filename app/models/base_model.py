cd2f4c1e0b27bf15d8e9cd77405d69ff0bba7d9f
XAntonioLuna <lunagantonio@gmail.com>
2024-03-12 17:03:05 -0700

    Initial project setup

---
+++ app/models/base_model.py
@@ -0,0 +1,11 @@
+"""
+This is a dummy base model created to simulate a database model that would exist in a real application.
+"""
+
+
+class BaseModel:
+    def save(self):
+        pass
+
+    def get(self, **kwargs):
+        pass
